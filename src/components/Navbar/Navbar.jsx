ac52355592f0af6930cf3203f2b1aeae7551e9e0
tryanimeshpanda <tryanimeshpanda58@gmail.com>
2023-04-01 03:01:30 +0530

    Update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
         <li className="p__opensans"><a href="#contact">Contact</a></li>
       </ul>
       <div className="app__navbar-login">
-        <a href="#login" className="p__opensans">Log In / Registration</a>
+        <a href="/" className="p__opensans">Log In / Registration</a>
         <div />
         <a href="/" className="p__opensans">Book Table</a>
       </div>
